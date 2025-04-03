4a56c506f78f6949fd2b8713f3fefbe886dd19da
nuexq <hi.yassir.op@gmail.com>
2025-04-03 14:38:54 +0000

    adding comments + adding more unwanted characters

--- src/commands.rs
+++ src/commands.rs
@@ -1,22 +1,28 @@
-use std::collections::HashMap;
-use std::fs;
-use std::path::PathBuf;
+use std::{collections::HashMap, fs, path::PathBuf};
 
 pub fn count_words(target: &PathBuf) {
     println!("Counting words in file {}", target.display());
+
     match fs::read_to_string(target) {
         Ok(text) => {
             let mut word_count: HashMap<String, u32> = HashMap::new();
+
             for word in text.split_whitespace() {
+                // removing unwanted characters from the word
+                let cleaned_word = word.trim_matches(['.', ',', '!', '?', ';', ':', '"', '\'']);
+
+                // Increment word count
                 word_count
-                    .entry(word.trim_matches(['.', ',', '!', '?']).to_string())
+                    .entry(cleaned_word.to_string())
                     .and_modify(|e| *e += 1)
                     .or_insert(1);
             }
 
+            // Sort word count by count
             let mut sorted_word_count: Vec<(_, _)> = word_count.into_iter().collect();
             sorted_word_count.sort_by(|a, b| b.1.cmp(&a.1));
 
+            // print sorted word count
             for (word, count) in sorted_word_count {
                 println!("{}: {}", word, count);
             }
@@ -27,4 +33,3 @@ pub fn count_words(target: &PathBuf) {
         }
     }
 }
-
